006c2d34dc4f2b711ab9890007c136d9daf606df
unknown <jair.lopes@fatec.sp.gov.br>
2021-10-25 20:22:04 -0300

    Alterado arquivo database

--- src/database/database.providers.ts
+++ src/database/database.providers.ts
@@ -5,11 +5,11 @@ export const databaseProviders = [
         provide: 'DATABASE_CONNECTION',
         useFactory: async () => await createConnection({
             type: 'postgres',
-            host: 'localhost',
+            host: 'ec2-34-196-34-142.compute-1.amazonaws.com',
             port: 5432,
-            username: 'postgres',
-            password: 'postgres',
-            database: 'pidb',
+            username: 'tmxpexudwgjbld',
+            password: '5211c2573fdb575ea26143fb10cb7bddd5389afb962817b0bad7446c5d8c61af',
+            database: 'd1eio5rcnfg6od',
             entities: [
                 __dirname + '/../**/*.entity{.ts,.js}',
             ],
