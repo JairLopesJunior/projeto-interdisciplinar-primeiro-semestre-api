0006c06cef4bf3c634bd98f97fb916ccd24a6df8
unknown <jair.lopes@fatec.sp.gov.br>
2021-10-25 20:28:43 -0300

    Alterado arquivo database

--- src/database/database.providers.ts
+++ src/database/database.providers.ts
@@ -7,6 +7,7 @@ export const databaseProviders = [
             type: 'postgres',
             host: 'ec2-34-196-34-142.compute-1.amazonaws.com',
             port: 5432,
+	    url: 'postgres://tmxpexudwgjbld:5211c2573fdb575ea26143fb10cb7bddd5389afb962817b0bad7446c5d8c61af@ec2-34-196-34-142.compute-1.amazonaws.com:5432/d1eio5rcnfg6od',
             username: 'tmxpexudwgjbld',
             password: '5211c2573fdb575ea26143fb10cb7bddd5389afb962817b0bad7446c5d8c61af',
             database: 'd1eio5rcnfg6od',
